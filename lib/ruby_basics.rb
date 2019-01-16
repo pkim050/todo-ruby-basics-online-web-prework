c6d42f6c4021fb7e0cf0dea2356a14e2911569e7
Patrick Kim <pkim050@gmail.com>
2019-01-16 03:41:22 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -16,12 +16,12 @@ def greeting(greeting, name)
   puts "#{greeting} #{name}!"
 end
 
-def return_a_value(value)
-  return value
+def return_a_value
+  return "Nice"
 end
 
-def last_evaluated_value(value)
-  value
+def last_evaluated_value
+  "expert"
 end
 
 def pizza_party(topping = "cheese")
